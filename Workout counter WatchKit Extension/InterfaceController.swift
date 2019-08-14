ba6b5080aa2f0d6ede9646cbf27db7a4dfd79c6b
jeffmellis <jeffmellis74@gmail.com>
2019-08-14 09:08:59 -0700

    Initial Commit

---
+++ Workout counter WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Workout counter WatchKit Extension
+//
+//  Created by jeff mellis on 2019-08-14.
+//  Copyright © 2019 jeff mellis. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Workout counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Workout counter
+//
+//  Created by jeff mellis on 2019-08-14.
+//  Copyright © 2019 jeff mellis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
